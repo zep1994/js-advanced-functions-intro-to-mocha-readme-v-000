fef9fdabdbab5b6a1e6d7662b7a44c56631e47d1
zep1994 <snaxbags@protonmail.com>
2018-11-04 17:10:07 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function sayHey() {
+  return 'Hey!';
+}
+
+function theTruth() {
+  return true;
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(flavor) {
+  return 'I love '+ flavor;
+}
+
+function shouting(word) {
+  return word.toUpperCase();
+}
+
+function roundDown(num) {
+  return Math.floor(num);
+}
